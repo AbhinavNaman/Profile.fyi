1ea7b97c59c792d7797e3c24fb23d205f72d1f40
AbhinavNaman <abhinavnaman3@gmail.com>
2024-08-15 15:30:48 +0530

    m-20

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       <html lang="en">
         <body>
           <Navbar />
-          <div className="mt-16">
+          <div className="m-20 mt-16">
           {children}
           </div>
           {/* <Footer/> */}
